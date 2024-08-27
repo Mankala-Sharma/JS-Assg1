b5c44861da12f081ff0e6442ebf142093e644273
Mankala Sharma <sharma.mankala06@gmail.com>
2024-08-27 20:59:06 +0530

    Add files via upload

---
+++ JS-Assg1/index.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+
+//Answe1
+console.log("JavaScript Primitive Data Types Examples:");
+
+
+let num = 2; // Integer 
+let str = "Hello"; // String
+let isEqual=true; //Boolean
+let age=null; //null or undefined
+
+console.log(num);
+console.log(str);
+console.log(isEqual);
+console.log(age);
+
+console.log("JavaScript Non-Primitive Data Types Examples are Object, Array, and Function.");
+
+
+//Answer2
